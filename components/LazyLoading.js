358474c1c54d5fbdcd5addcce5be2c2ccfd22d42
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2024-09-22 00:16:24 +0530

    Update LazyLoading.js

--- components/LazyLoading.js
+++ components/LazyLoading.js
@@ -21,8 +21,8 @@ const LazyLoading = ({ onLoadingComplete }) => {
 
   return (
     <div className="fixed inset-0 bg-[#AA172C] flex flex-col items-center justify-center">
-      <div className="w-32 h-32 bg-white rounded-full flex items-center justify-center mb-8 overflow-hidden">
-        <div className="w-24 h-24 relative">
+      <div className="w-64 h-64 bg-white rounded-full flex items-center justify-center mb-8 overflow-hidden">
+        <div className="w-56 h-56 relative">
           <Image
             src="/images/conv-logo.png"
             alt="Logo"
@@ -33,13 +33,12 @@ const LazyLoading = ({ onLoadingComplete }) => {
         </div>
       </div>
       <div className="text-white text-4xl font-bold mb-4">{progress}%</div>
-      <div className="w-64 h-2 bg-[#8A1323] rounded-full overflow-hidden">
+      <div className="w-3/4 h-1 bg-[#8A1323] rounded-full overflow-hidden">
         <div
           className="h-full bg-white rounded-full transition-all duration-300 ease-out"
           style={{ width: `${progress}%` }}
         ></div>
       </div>
-      <div className="mt-4 text-white text-xl font-light">NAGATIO | SOLITIO | ACTIO</div>
     </div>
   );
 };
