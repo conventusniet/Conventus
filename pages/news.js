81bf8311ef326c3f36983f250e25a214d3be0374
Anubhav Singh <singhanubhav7456@gmail.com>
2024-10-10 00:24:20 +0530

    Update news.js

--- pages/news.js
+++ pages/news.js
@@ -46,7 +46,7 @@ const NewsletterCard = ({ semester, imageUrl, pdfUrl }) => {
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="relative w-full aspect-[3/4] mx-auto overflow-hidden rounded-xl shadow-2xl"
+      className="relative w-full aspect-[3/4] mx-auto overflow-hidden rounded-xl"
     >
       <Image
         src={imageUrl}
@@ -55,7 +55,7 @@ const NewsletterCard = ({ semester, imageUrl, pdfUrl }) => {
         objectFit="cover"
         className="transition-transform duration-300 hover:scale-105"
       />
-      <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-6">
+      <div className="absolute bottom-0 left-0 right-0 p-6">
         <h3 className="text-2xl font-semibold text-white mb-4">
           {semester} Semester
         </h3>
