9eca13de24c485d69cb8ed57ece4bbc0f7f7f0f5
YASHRAJ RANJAN <yashrajranjan29@gmail.com>
2024-10-14 12:01:23 +0530

    Update mun1.0.js pics positions

--- pages/mun1.0.js
+++ pages/mun1.0.js
@@ -67,10 +67,10 @@ const MediaGallery = () => {
     '/images/coll2.jpg', '/images/coll4.jpg', '/images/commaippm.jpg', '/images/coll6.jpg',
     '/images/photography.jpg.jpg', '/images/caricature.JPG', '/images/coll3.jpg',
     '/images/coll5.jpg', '/images/coll7.jpg', '/images/coll8.jpg','/images/discussions.JPG',
-    '/images/secretariat1.JPG','/images/upcomingmun2.jpg' ,'/images/newsletterreveal2.jpg', '/images/bjpvscongress.jpg','/images/pushkapurv.jpg',
-    '/images/dakshunsc.JPG', '/images/ip1.JPG','/images/indiapak2.JPG', '/images/indiapak.JPG', '/images/secretariat2.jpg', '/images/secretariat4.JPG',
-    '/images/secretariat5.JPG', '/images/unsc1.jpg', '/images/uncswbangladesh.JPG','/images/sujalyashrajvaibhav.jpg','/images/blurfromback.jpg',
-    '/images/solodakshunsc.jpg','/images/solobangladeshuncsw.jpg','/images/solomodi.jpg','/images/soloindiauncsw.jpg','/images/solonewzealanduncsw.jpg','/images/solopragya.jpg','/images/solorussiauncsw.jpg','/images/soloshagun.jpg','/images/solousaunsc.jpg','/images/manaseb.jpg',
+    '/images/upcomingmun2.jpg',
+    '/images/dakshunsc.JPG', '/images/ip1.JPG', '/images/indiapak.JPG','/images/soloindiauncsw.jpg', '/images/secretariat2.jpg', '/images/secretariat4.JPG',
+     '/images/unsc1.jpg', '/images/uncswbangladesh.JPG','/images/indiapak2.JPG','/images/sujalyashrajvaibhav.jpg','/images/blurfromback.jpg','/images/secretariat5.JPG',
+    '/images/solodakshunsc.jpg','/images/solobangladeshuncsw.jpg','/images/solomodi.jpg','/images/solonewzealanduncsw.jpg','/images/solopragya.jpg','/images/solorussiauncsw.jpg','/images/soloshagun.jpg','/images/solousaunsc.jpg','/images/manaseb.jpg',
   ];
 
   return (
