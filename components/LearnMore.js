0b84b32861f6a00b99fc40214b3e207277d77c05
Manas Gupta <116839659+14ManasGupta@users.noreply.github.com>
2024-10-19 21:48:33 +0530

    Update LearnMore.js

--- components/LearnMore.js
+++ components/LearnMore.js
@@ -10,19 +10,19 @@ const LearnMoreSection = () => {
         {
             title: "About Us",
             description: "Know more about our vision and mission",
-            image: "/images/3.jpg",
+            image: "/images/h3.jpg",
             route: "/aboutus"
         },
         {
             title: "Model United Nations 2025",
             description: "Explore the Second Edition of Conventus MUN",
-            image: "/images/5.jpg",
+            image: "/images/h4.jpg",
             route: "/mun1.0"
         },
         {
             title: "Registration",
             description: "Registration are now open, Secure your front-row seat to diplomacy now!",
-            image: "/images/4.jpg",
+            image: "/images/h5.jpg",
             route: "/registration"
         }
     ];
