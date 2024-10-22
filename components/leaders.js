57e7f2c53e96db9f2e009e3191e983e3fedbaa65
Revant <86226975+revant7@users.noreply.github.com>
2024-10-22 20:13:18 +0530

    Added Image

--- components/leaders.js
+++ components/leaders.js
@@ -43,7 +43,7 @@ const LeadershipPage = () => {
         {
             name: "Revant Khanna",
             role: "Technical Co-Head",
-            imageUrl: "/images/sanskar.jpg",
+            imageUrl: "/images/Revant.jpg",
             branch: "CSE - AI"
         },
     ];
