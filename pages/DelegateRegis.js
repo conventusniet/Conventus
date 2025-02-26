d4ba02bffb4f77b3f151ab7e6fa1f86403608f4c
Revant <86226975+revant7@users.noreply.github.com>
2025-02-26 21:34:27 +0530

    Changed Back To Download Matrix

--- pages/DelegateRegis.js
+++ pages/DelegateRegis.js
@@ -327,11 +327,11 @@ const DelegateRegistrationForm = () => {
                     <p className="text-gray-700 mb-4 text-justify">Addressing Security Crises and Regional Instability in Middle East while promoting reconstruction Governance and peace negotiations among key stakeholders.</p>
                     <div className="mb-4 flex items-center justify-center space-x-4">
                         <button
-                            onClick={() => window.open('https://docs.google.com/spreadsheets/d/143eDDNCPQHaGE-jOL5oBKuZIGTFNt2uB/edit?usp=sharing&ouid=105922611711816574457&rtpof=true&sd=true', '_blank')}
+                            onClick={() => handleMatrixDownload('/Matrix/United Nations Security Council.xlsx')}
                             className="flex items-center text-red-600 hover:text-red-700"
                         >
-                            <Eye className="mr-2" size={20} />
-                            View Matrix
+                            <Download className="mr-2" size={20} />
+                            Download Matrix
                         </button>
 
                     </div>
@@ -339,22 +339,22 @@ const DelegateRegistrationForm = () => {
                     <p className="text-gray-700 mb-4 text-justify">Rights of Refugees and Asylum Seekers Amidst Global Migration Crises: Balancing sovereignty with humanitarian responsibilities.</p>
                     <div className="mb-4 flex items-center justify-center space-x-4">
                         <button
-                            onClick={() => window.open('https://docs.google.com/spreadsheets/d/14RhNt0rqxsM_SU8Bcmkpw47sc2R1uKtg/edit?usp=sharing&ouid=105922611711816574457&rtpof=true&sd=true', '_blank')}
+                            onClick={() => handleMatrixDownload('/Matrix/UN Human Rights Council.xlsx')}
                             className="flex items-center text-red-600 hover:text-red-700"
                         >
-                            <Eye className="mr-2" size={20} />
-                            View Matrix
+                            <Download className="mr-2" size={20} />
+                            Download Matrix
                         </button>
                     </div>
                     <h5 className="text-xl font-semibold mb-2">All India Political Party Meet</h5>
                     <p className="text-gray-700 mb-4 text-justify">One Nation, One Election: Evaluating the feasibility, impact on governance, and democratic implications of simultaneous elections while balancing federalism.</p>
                     <div className="mb-4 flex items-center justify-center space-x-4">
                         <button
-                            onClick={() => window.open('https://docs.google.com/spreadsheets/d/1v24pWSGc2l_dpdxBVMlfY6X5oo2Bx9T6/edit?usp=drive_link&ouid=105922611711816574457&rtpof=true&sd=true', '_blank')}
+                            onClick={() => handleMatrixDownload('/Matrix/All India Political Party Meet.xlsx')}
                             className="flex items-center text-red-600 hover:text-red-700"
                         >
-                            <Eye className="mr-2" size={20} />
-                            View Matrix
+                            <Download className="mr-2" size={20} />
+                            Download Matrix
                         </button>
                     </div>
                     <h5 className="text-xl font-semibold mb-2">IP</h5>
